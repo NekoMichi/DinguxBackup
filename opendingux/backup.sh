c024b2a3dd7713a84e9136171a43494cb39aaf73
NekoMichi <31323046+NekoMichi@users.noreply.github.com>
2020-07-06 19:17:42 +0100

    Skip nonexistent folders
    Skips backing up any folders associated with emulators not used. Should speed things up a little.

--- opendingux/backup.sh
+++ opendingux/backup.sh
@@ -27,73 +27,94 @@ echo "Backing up Gmenu2x data"
 rsync --update -r /media/data/local/home/.gmenu2x/ /media/sdcard/backup/gmenu2x
 
 # Backs up screenshots
-if [ ! -d /media/sdcard/backup/screenshots/ ]; then
-	echo "Screenshots backup folder doesn't exist, creating folder..."
-	mkdir /media/sdcard/backup/screenshots
+if [ -d /media/data/local/home/screenshots/ ]; then
+	if [ ! -d /media/sdcard/backup/screenshots/ ]; then
+		echo "Screenshots backup folder doesn't exist, creating folder..."
+		mkdir /media/sdcard/backup/screenshots
+	fi
+	echo "Backing up screenshots"
+	rsync --update -r /media/data/local/home/screenshots/ /media/sdcard/backup/screenshots
 fi
-echo "Backing up screenshots"
-rsync --update -r /media/data/local/home/screenshots/ /media/sdcard/backup/screenshots
 
 # Backs up scripts
-if [ ! -d /media/sdcard/backup/scriptrunner/ ]; then
-	echo "Scripts backup folder doesn't exist, creating folder..."
-	mkdir /media/sdcard/backup/scriptrunner
+if [ -d /media/data/local/home/.scriptrunner/ ]; then
+	if [ ! -d /media/sdcard/backup/scriptrunner/ ]; then
+		echo "Scripts backup folder doesn't exist, creating folder..."
+		mkdir /media/sdcard/backup/scriptrunner
+	fi
+	echo "Backing up scripts"
+	rsync --update -r /media/data/local/home/.scriptrunner/ /media/sdcard/backup/scriptrunner
 fi
-echo "Backing up scripts"
-rsync --update -r /media/data/local/home/.scriptrunner/ /media/sdcard/backup/scriptrunner
 
 # Backs up FCEUX data
-if [ ! -d /media/sdcard/backup/fceux/ ]; then
-	echo "FCEUX backup folder doesn't exist, creating folder..."
-	mkdir /media/sdcard/backup/fceux
+if [ -d /media/data/local/home/.fceux/ ]; then
+	if [ ! -d /media/sdcard/backup/fceux/ ]; then
+		echo "FCEUX backup folder doesn't exist, creating folder..."
+		mkdir /media/sdcard/backup/fceux
+	fi
+	echo "Backing up FCEUX data"
+	rsync --update -r /media/data/local/home/.fceux/ /media/sdcard/backup/fceux
 fi
-echo "Backing up FCEUX data"
-rsync --update -r /media/data/local/home/.fceux/ /media/sdcard/backup/fceux
 
 # Backs up Gambatte data
-if [ ! -d /media/sdcard/backup/gambatte/ ]; then
-	echo "Gambatte backup folder doesn't exist, creating folder..."
-	mkdir /media/sdcard/backup/gambatte
+if [ -d /media/data/local/home/.gambatte/ ]; then
+	if [ ! -d /media/sdcard/backup/gambatte/ ]; then
+		echo "Gambatte backup folder doesn't exist, creating folder..."
+		mkdir /media/sdcard/backup/gambatte
+	fi
+	echo "Backing up Gambatte data"
+	rsync --update -r /media/data/local/home/.gambatte/ /media/sdcard/backup/gambatte
 fi
-echo "Backing up Gambatte data"
-rsync --update -r /media/data/local/home/.gambatte/ /media/sdcard/backup/gambatte
 
 # Backs up ReGBA data
-if [ ! -d /media/sdcard/backup/gpsp/ ]; then
-	echo "ReGBA backup folder doesn't exist, creating folder..."
-	mkdir /media/sdcard/backup/gpsp
+if [ -d /media/data/local/home/.gpsp/ ]; then
+	if [ ! -d /media/sdcard/backup/gpsp/ ]; then
+		echo "ReGBA backup folder doesn't exist, creating folder..."
+		mkdir /media/sdcard/backup/gpsp
+	fi
+	echo "Backing up ReGBA data"
+	rsync --update -r /media/data/local/home/.gpsp/ /media/sdcard/backup/gpsp
 fi
-echo "Backing up ReGBA data"
-rsync --update -r /media/data/local/home/.gpsp/ /media/sdcard/backup/gpsp
 
 # Backs up PCSX4all data
-if [ ! -d /media/sdcard/backup/pcsx4all/ ]; then
-	echo "PCSX4all backup folder doesn't exist, creating folder..."
-	mkdir /media/sdcard/backup/pcsx4all
+if [ -d /media/data/local/home/.pcsx4all/ ]; then
+	if [ ! -d /media/sdcard/backup/pcsx4all/ ]; then
+		echo "PCSX4all backup folder doesn't exist, creating folder..."
+		mkdir /media/sdcard/backup/pcsx4all
+	fi
+	echo "Backing up PCSX4all data"
+	rsync --update -r /media/data/local/home/.pcsx4all/ /media/sdcard/backup/pcsx4all
 fi
-echo "Backing up PCSX4all data"
-rsync --update -r /media/data/local/home/.pcsx4all/ /media/sdcard/backup/pcsx4all
 
 # Backs up Picodrive data
-if [ ! -d /media/sdcard/backup/picodrive/ ]; then
-	echo "Picodrive backup folder doesn't exist, creating folder..."
-	mkdir /media/sdcard/backup/picodrive
+if [ -d /media/data/local/home/.picodrive/ ]; then
+	if [ ! -d /media/sdcard/backup/picodrive/ ]; then
+		echo "Picodrive backup folder doesn't exist, creating folder..."
+		mkdir /media/sdcard/backup/picodrive
+	fi
+	echo "Backing up PicoDrive data"
+	rsync --update -r /media/data/local/home/.picodrive/ /media/sdcard/backup/picodrive
 fi
-echo "Backing up PicoDrive data"
-rsync --update -r /media/data/local/home/.picodrive/ /media/sdcard/backup/picodrive
 
 # Backs up PocketSNES data
-if [ ! -d /media/sdcard/backup/snes96_snapshots/ ]; then
-	echo "SNES96 backup folder doesn't exist, creating folder..."
-	mkdir /media/sdcard/backup/snes96_snapshots
+if [ -d /media/data/local/home/.snes96_snapshots/ ]; then
+	if [ ! -d /media/sdcard/backup/snes96_snapshots/ ]; then
+		echo "SNES96 backup folder doesn't exist, creating folder..."
+		mkdir /media/sdcard/backup/snes96_snapshots
+	fi
+	echo "Backing up SNES96 data"
+	rsync --update -r /media/data/local/home/.snes96_snapshots/ /media/sdcard/backup/snes96_snapshots
 fi
-if [ ! -d /media/sdcard/backup/pocketsnes/ ]; then
-	echo "PocketSNES backup folder doesn't exist, creating folder..."
-	mkdir /media/sdcard/backup/pocketsnes
+
+if [ -d /media/data/local/home/.pocketsnes/ ]; then
+	if [ ! -d /media/sdcard/backup/pocketsnes/ ]; then
+		echo "PocketSNES backup folder doesn't exist, creating folder..."
+		mkdir /media/sdcard/backup/pocketsnes
+	fi
+	echo "Backing up PocketSNES data"
+	rsync --update -r /media/data/local/home/.pocketsnes/ /media/sdcard/backup/pocketsnes
 fi
-echo "Backing up PocketSNES data"
-rsync --update -r /media/data/local/home/.pocketsnes/ /media/sdcard/backup/pocketsnes
-rsync --update -r /media/data/local/home/.snes96_snapshots/ /media/sdcard/backup/snes96_snapshots
+
 echo ""
 read -p "Backup complete! Press START to exit."
 exit
